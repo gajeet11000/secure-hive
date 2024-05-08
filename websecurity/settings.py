0019cbb75d7e942bc226f662043ef70a34738b28
G Ajeet <gajeet11000@gmail.com>
2024-05-08 09:55:33 +0530

    Update settings.py last

--- websecurity/settings.py
+++ websecurity/settings.py
@@ -21,9 +21,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["secure-hive.onrender.com", "*"]
 
-#CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ["https://secure-hive.onrender.com"]
 
 
 
